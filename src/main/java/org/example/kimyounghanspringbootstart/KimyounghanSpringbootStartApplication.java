75e061579ea391b8552b45f8fd6b2cd72bbe10ed
hajju0617 <joowon023617@gmail.com>
2024-08-16 00:22:58 +0900

    springboot

---
+++ src/main/java/org/example/kimyounghanspringbootstart/KimyounghanSpringbootStartApplication.java
@@ -0,0 +1,13 @@
+package org.example.kimyounghanspringbootstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KimyounghanSpringbootStartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KimyounghanSpringbootStartApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/kimyounghanspringbootstart/KimyounghanSpringbootStartApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.kimyounghanspringbootstart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KimyounghanSpringbootStartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
